7dc7f5d108431b40d74e40cc05bd5d669118e1b0
Julian Fleischer <tirednesscankill@warhog.net>
2021-01-24 22:03:05 +0100

    Bump version -> 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dezimal",
-    version="0.3.0",
+    version="0.4.0",
     author="Julian Fleischer",
     author_email="tirednesscankill@warhog.net",
     description="Arbitrary precision decimal numbers",
